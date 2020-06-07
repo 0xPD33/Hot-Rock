3cfaa6746287c63af4ff07d5f06a18bc4e965d0d
EnrgyPC <Patrick.Gebhardt99@gmail.com>
2020-06-07 17:36:04 +0200

    added gem sound effects and a vanish animation on collect

--- Scripts/Gem.gd
+++ Scripts/Gem.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
+var collected = false
+
 
 func _ready():
 	add_to_group("Gems")
 
 
 func _on_Area2D_body_entered(body: Node):
-	if body is KinematicBody2D:
+	# check if it's the player and check if the gem has already been 
+	# collected to prevent the gem from being collected twice
+	if body is KinematicBody2D and !collected:
+		collected = true
+		vanish()
+		yield($VanishAnimation, "animation_finished")
 		get_tree().call_group("Game", "_on_gem_collected")
 		queue_free()
+
+
+func vanish():
+	$AudioStreamPlayer2D.play()
+	$VanishAnimation.play("gem_vanish")
 

4c1e4fb0539527643789d8d6f5ec25a764b2c816
EnrgyPC <Patrick.Gebhardt99@gmail.com>
2020-06-07 18:05:37 +0200

    fixed some stuff, added lots of textures for upcoming UI revamp

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -9,7 +9,6 @@ func _ready():
 
 func _on_PlayButton_pressed():
 	AnimPlayer.play("fade_out")
-	
 	# waits for the fade_out animation to finish before changing to the level scene
 	yield(AnimPlayer, "animation_finished")
 	get_tree().change_scene("res://Scenes/Levels/Level1.tscn")
